8480f65cdfd671fa47d3d4b3f86cfbd7412cf7d7
Pedro <pedro.almeida.perez@gmail.com>
2019-06-17 17:05:46 +0200

    Clean Library from comments and code that is not use

--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -1,24 +1,15 @@
 package model;
 
-
-
 import utils.BookConfiguration;
 import utils.PersonConfiguration;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Library {
-   // private static ArrayList<Person> person;
-    //private static LinkedList<Book> book;
     private BookConfiguration bookConfiguration;
     private PersonConfiguration personConfiguration;
 
     public Library() {
         bookConfiguration = new BookConfiguration();
         personConfiguration = new PersonConfiguration();
-      //   book = new LinkedList();
-        // person = new ArrayList();
     }
 
     public BookConfiguration getBookConfiguration() {
@@ -36,21 +27,4 @@ public class Library {
     public void setPersonConfiguration(PersonConfiguration personConfiguration) {
         this.personConfiguration = personConfiguration;
     }
-    /* public LinkedList<Book> getBook() {
-        return book;
-    }
-
-    public void setBook(LinkedList<Book> book) {
-        Library.book = book;
-    }*/
-
-   /* public  ArrayList<Person> getPerson() {
-        return person;
-    }
-
-    public void setPerson(ArrayList<Person> person) {
-        Library.person = person;
-    }
-*/
-
 }
