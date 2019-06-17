d0fc521ba020d47e13994105bbc00b35e4f785df
Pedro <pedro.almeida.perez@gmail.com>
2019-06-17 18:28:55 +0200

    Add Unit Test to test size from ArrayList <Person>

--- src/main/java/data/DataBook.java
+++ src/main/java/data/DataBook.java
@@ -5,18 +5,18 @@ import model.Book;
 import java.util.LinkedList;
 
 public class DataBook {
-    public static void dataInsert(LinkedList<Book> book){
-        book.add(new Book("Luces de bohemia", "Ramón", 1998));
-        book.add(new Book("Luces de bohemia", "Ramón", 1998));
-        book.add(new Book("Crimen y castigo", "Fyodor", 1998));
-        book.add(new Book("Las almas muertas", "Nicolai", 1998));
-        book.add(new Book("Preludio a la fundación", "Fyodor", 1998));
-        book.add(new Book("Crimen", "Fyodor", 1984));
-        book.add(new Book("Crimen2", "Fyodor", 1998));
-        book.add(new Book("Crimen3", "Fyodor", 1984));
-        book.add(new Book("Crimen", "Fyodor", 1998));
-        book.add(new Book("Crimen", "Fyodor", 1999));
-        book.add(new Book("Crimen", "Fyodor", 2000));
-        book.add(new Book("Crimen", "Fyodor", 1999));
+    public static void dataInsert(LinkedList<Book> listBook){
+        listBook.add(new Book("Luces de bohemia", "Ramón", 1998));
+        listBook.add(new Book("Luces de bohemia", "Ramón", 1998));
+        listBook.add(new Book("Crimen y castigo", "Fyodor", 1998));
+        listBook.add(new Book("Las almas muertas", "Nicolai", 1998));
+        listBook.add(new Book("Preludio a la fundación", "Fyodor", 1998));
+        listBook.add(new Book("Crimen", "Fyodor", 1984));
+        listBook.add(new Book("Crimen2", "Fyodor", 1998));
+        listBook.add(new Book("Crimen3", "Fyodor", 1984));
+        listBook.add(new Book("Crimen", "Fyodor", 1998));
+        listBook.add(new Book("Crimen", "Fyodor", 1999));
+        listBook.add(new Book("Crimen", "Fyodor", 2000));
+        listBook.add(new Book("Crimen", "Fyodor", 1999));
     }
 }
--- src/test/java/utils/PersonConfigurationTest.java
+++ src/test/java/utils/PersonConfigurationTest.java
@@ -1,14 +1,21 @@
 package utils;
 
+import model.Book;
+import model.Person;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class PersonConfigurationTest {
-
+    private static ArrayList<Person> listPerson = new ArrayList<Person>();
     @Test
     public void addPersons() {
-
+        listPerson.add(new Person("Ramón"));
+        int result = 1;
+        Assert.assertEquals(result,listPerson.size() );
     }
 
     @Test
