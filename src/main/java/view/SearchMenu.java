be090904eb9999bb9a65333e8ae6aa5672ca3133
Pedro <pedro.almeida.perez@gmail.com>
2019-06-18 16:18:45 +0200

    Add try catch in the SearchMenu

--- src/main/java/view/SearchMenu.java
+++ src/main/java/view/SearchMenu.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class SearchMenu {
     private int option;
     public int selectOption (){
-        Scanner scanner = new Scanner(System.in);
-        //while (option != 0){
-        System.out.println("*-----------Welcome to ATOS Search Menu------------------*");
-        System.out.println("  Press 1 Search by tittle: ");
-        System.out.println("  Press 2 Search by author: ");
-        System.out.println("  Press 3 Search by year: ");
-        System.out.println("  Press 4 Search by author and tittle: ");
-        System.out.println("  Press 9 to go to Main menu");
-        System.out.println("*--------------------------------------------------------*");
+        try{
+            Scanner scanner = new Scanner(System.in);
+            //while (option != 0){
+            System.out.println("*-----------Welcome to ATOS Search Menu------------------*");
+            System.out.println("  Press 1 Search by tittle: ");
+            System.out.println("  Press 2 Search by author: ");
+            System.out.println("  Press 3 Search by year: ");
+            System.out.println("  Press 4 Search by author and tittle: ");
+            System.out.println("  Press 9 to go to Main menu");
+            System.out.println("*--------------------------------------------------------*");
 
-        option = scanner.nextInt();
-        //}
+            option = scanner.nextInt();
+            //}
 
-        return option;
+            return option;
+        }catch (Exception e){
+            return 99;
+        }
+
     }
 }
